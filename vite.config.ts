6cad88024e3bcc22339a63a25106f82194932996
naweldayrek29-cpu <naweldayrek29@gmail.com>
2025-10-01 14:14:19 +0200

    Publish build to docs + correct Vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Nawel-Dayrek/", // 👈 ajoute ça
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
